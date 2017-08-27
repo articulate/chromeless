19ea19203608654908c5ef323bdfabdf687b12e6
Patrick Klingemann <patrick.klingemann@gmail.com>
2017-08-27 16:39:49 -0400

    @articulate/chromeless@1.3.0 beta4 (#9)
    * bump @articulate/chromeless to 1.3.0-beta4
    
    * better test file

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 const { default: Chromeless } = require('./dist/src')
 
-new Chromeless({ remote: true })
-  .goto('http://html5zombo.com')
-  .screenshot()
-  .then(console.log)
-  .catch(console.error)
+for (var i=0; i<100; i++) {
+  new Chromeless({ remote: true })
+    .goto('http://html5zombo.com')
+    .screenshot()
+    .end()
+    .then(console.log)
+    .catch(console.error)
 
-new Chromeless({ remote: true })
-  .goto('http://html5zombo.com')
-  .pdf()
-  .then(console.log)
-  .catch(console.error)
+  new Chromeless({ remote: true })
+    .goto('http://html5zombo.com')
+    .pdf()
+    .end()
+    .then(console.log)
+    .catch(console.error)
+}
